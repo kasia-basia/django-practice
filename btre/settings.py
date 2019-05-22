fb1ae74ee90ba7b9941deb8d5af7e2d83e701709
Kasia Lach <lachkasia4@gmail.com>
2019-05-22 20:02:36 +0200

    Added Realtor and Listing models

--- btre/settings.py
+++ btre/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# Instead of default sqlite.db, we use Postgres
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btredb',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost'
     }
 }
 
--- realtors/models.py
+++ realtors/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+
+
+class Realtor(models.Model):
+    name = models.CharField(max_length=200)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    description = models.TextField(blank=True)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField()
+    is_mvp = models.BooleanField(default=False)
+    hire_date = models.DateField(default=datetime.now)
+
+    def __str__(self):
+        return self.name
